9c5e1fdab189a5518dcd0153acb20786565f10fb
Anastasiya Simonenko <avsimonenko@esc-center.ru>
2017-07-12 21:37:31 +0300

    Change standard output by file output

--- main.cpp
+++ main.cpp
@@ -12,10 +12,27 @@
   (void)select(0, NULL, NULL, NULL, &wait);
 #endif
 
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+    return written;
+}
+
 void initCurlOptions() {
     curl_global_init(CURL_GLOBAL_DEFAULT);
     CURL *http_handle = curl_easy_init();
-    curl_easy_setopt(http_handle, CURLOPT_URL, "http://www.example.com/");
+
+    static const char *pagefilename = "image";
+
+    curl_easy_setopt(http_handle, CURLOPT_URL, "https://lh4.ggpht.com/M1XTibfCgpi5pgjSDb7kXDh21N8fpn-8evzQVAX-qGFhSyArDmSuCAv1pjVp4jbAt_g=h900");
+    curl_easy_setopt(http_handle, CURLOPT_WRITEFUNCTION, write_data);
+
+    FILE *pagefile;
+    pagefile = fopen(pagefilename, "wb");
+    if(pagefile) {
+        /* write the page body to this file handle */
+        curl_easy_setopt(http_handle, CURLOPT_WRITEDATA, pagefile);
+    }
     CURLM *multi_handle = curl_multi_init();
     curl_multi_add_handle(multi_handle, http_handle);
 
@@ -58,6 +75,8 @@ void initCurlOptions() {
     curl_multi_cleanup(multi_handle);
 
     curl_global_cleanup();
+
+    fclose(pagefile);
 };
 
 int main(int argc, char *argv[]) {
